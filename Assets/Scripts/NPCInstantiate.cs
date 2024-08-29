975865e10eebd893b505b8fca6d23edab2e962e1
M.R <m.r.MeijoUniv@gmail.com>
2024-08-29 13:30:23 +0900

    [update] NPCを生成するスクリプトの一部変更

--- Assets/Scripts/NPCInstantiate.cs
+++ Assets/Scripts/NPCInstantiate.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class NPCInstantiate : MonoBehaviour
 {
     [SerializeField] private GameObject _npcPrefab;
-    [SerializeField] private Transform _minPoint;
-    [SerializeField] private Transform _maxPoint;
+    [SerializeField] private GameObject[] _npcMovePoints = new GameObject[3];
+    
     public int NPCCount;
 
     private void Start()
     {
+        SetNPCNextPoint(1);
+    }
+
+    private void SetNPCNextPoint(int pointNum)
+    {
+        if(pointNum > 1)
+        {
+            _npcMovePoints[pointNum - 2].SetActive(false);
+        }
+        _npcMovePoints[pointNum - 1].SetActive(true);
+
+        Transform _minPoint = _npcMovePoints[pointNum - 1].transform.GetChild(0);
+        Transform _maxPoint = _npcMovePoints[pointNum - 1].transform.GetChild(1);
         for(int i = 0; i < NPCCount; i++)
         {
-            Instantiate(_npcPrefab, new Vector3(Random.Range(_minPoint.position.x, _maxPoint.position.x), 0f, Random.Range(_minPoint.position.z, _maxPoint.position.z)), Quaternion.identity);
+            Instantiate(_npcPrefab, 
+                        new Vector3(Random.Range(_minPoint.position.x, _maxPoint.position.x), 0f, Random.Range(_minPoint.position.z, _maxPoint.position.z)), 
+                        Quaternion.identity, 
+                        this.transform);
         }
     }
 }
