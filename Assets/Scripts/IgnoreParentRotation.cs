ffca2c9132cae37c7ee2f182530b2fe5b7060086
M.R <m.r.MeijoUniv@gmail.com>
2024-10-14 11:52:30 +0900

    [update] ミニマップのため、ライトベイクのやり直し及びアイコンの変更

--- Assets/Scripts/IgnoreParentRotation.cs
+++ Assets/Scripts/IgnoreParentRotation.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class IgnoreParentRotation : MonoBehaviour
 {
-    
+    public float height = 150f;
+    public bool IsIgnoreRotation = true;
+    public Vector3 rotation = new Vector3(90f, -90f, 0f);
     
     private void Update()
     {
-        transform.position = new Vector3(transform.parent.position.x, 150f, transform.parent.position.z);
-        transform.rotation = Quaternion.Euler(90f, -90f, 0f);
+        transform.position = new Vector3(transform.parent.position.x, height, transform.parent.position.z);
+
+        if(IsIgnoreRotation)
+            transform.rotation = Quaternion.Euler(rotation);
+        else
+            transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
     }
 }
