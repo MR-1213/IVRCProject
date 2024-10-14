8b8470727d7c845d55ad9efb3aba3dbb2bd9c50d
IronRiver <ironriver2003@gmail.com>
2024-10-14 17:43:57 +0900

    [add] ScenarioEventManagerを追加
    GamePlayManagerのシナリオイベント処理を抜き出したコンポーネントを追加。まだ実用できるほど設計を詰め切っていないのでGamePlayManager側の変更は無し。

---
+++ Assets/Scripts/ScenarioEventManager.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+using System;
+
+#nullable enable
+
+public class ScenarioEventManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _textMesh;
+    [SerializeField]
+    private AudioSource _seAudioSource;
+    [SerializeField]
+    private AudioClip _feedSE;
+
+    [SerializeField]
+    private GameObject _meltPointMarker;
+    [SerializeField]
+    private GameObject[] _goalMarkers;
+
+    [SerializeField]
+    [TextArea]
+    private string _introductionText = string.Join("\n", new string[] {
+        "ここは名古屋駅桜通口付近です。",
+        "そしてあなたは大事な会議に遅刻しそうです...！",
+        "",
+        "向かうべきはあおなみ線の改札口です。",
+        "しかし、ここからは遠く、多くの人で混雑する名古屋駅では間に合うか分かりません...",
+        "ここはあなたの持つものを融かす能力を使って、あおなみ線までの近道を作ってしまいましょう！",
+        "",
+        "まずは名古屋駅構内に入り、<color=red>赤色</color>の目印まで向かいましょう！",
+        "",
+        "『移動方法』左スティックで移動・回転ができます。メニューボタンは押さないように注意してください！",
+    });
+
+    [SerializeField]
+    [TextArea]
+    private string _tutorialCompletedText = string.Join("\n", new string[] {
+        "素晴らしいです！この調子であおなみ線を目指しましょう！",
+        "",
+        "あおなみ線は左奥の<color=green>緑色</color>に光っている場所です！",
+        "赤色の目印の壁を融かして進んでいきましょう！",
+    });
+
+    [SerializeField]
+    [TextArea]
+    private string _gameCompletedText = string.Join("\n", new string[] {
+        "おめでとうございます！",
+        "あなたは無事にあおなみ線の改札口まで到着しました！",
+        "",
+        "体験いただきありがとうございました。",
+        "ヘッドセットを外してください。",
+    });
+
+    public void StartIntroduction(Action? callback = null)
+    {
+        StartCoroutine(PlayScenarioEvent(_introductionText, callback));
+    }
+
+    public void StartTutorialCompleted(Action? callback = null)
+    {
+        StartCoroutine(PlayScenarioEvent(_tutorialCompletedText, callback));
+    }
+
+    public void StartGameCompleted(Action? callback = null)
+    {
+        StartCoroutine(PlayScenarioEvent(_gameCompletedText, callback));
+    }
+
+    private IEnumerator PlayScenarioEvent(string text, Action? callback)
+    {
+        // TODO: キャンセル処理(コルーチンのキャンセル手法を考える or UniTask+CancellationTokenを使う)
+
+        // テキストUIを表示
+        _textMesh.gameObject.SetActive(true);
+        var isFirst = true;
+        var meltPointMarkerEnabled = false;
+        foreach (var line in text.Split('\n'))
+        {
+            if (line != string.Empty)
+            {
+                // ポイントの強調テキストに合わせてオブジェクトを表示する
+                if (line.Contains("<color=red>"))
+                {
+                    meltPointMarkerEnabled = true;
+                    _meltPointMarker.SetActive(true);
+                }
+                else if (meltPointMarkerEnabled)
+                {
+                    meltPointMarkerEnabled = false;
+                    _meltPointMarker.SetActive(false);
+                }
+
+                // ゴールを強調表示する
+                if (line.Contains("<color=green>"))
+                {
+                    foreach (var item in _goalMarkers)
+                    {
+                        item.layer = LayerMask.NameToLayer("WallHack");
+                    }
+                }
+
+                _textMesh.text += line;
+                continue;
+            }
+
+            // 文字送りを始める
+            _textMesh.text += "\n【トリガーボタンで次へ】";
+            // 前フレームの入力が残っていないか確認
+            if (!isFirst && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+            {
+                yield return null;
+            }
+            // 入力を待機
+            while (!OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+            {
+                yield return null;
+            }
+            if (isFirst)
+            {
+                isFirst = false;
+            }
+
+            // テキストをクリア
+            _textMesh.text = "";
+            // SEを再生
+            _seAudioSource.PlayOneShot(_feedSE);
+        }
+
+        if (meltPointMarkerEnabled)
+        {
+            _meltPointMarker.SetActive(false);
+        }
+
+        // テキストUIを非表示
+        _textMesh.gameObject.SetActive(false);
+        // コールバックを起動
+        callback?.Invoke();
+    }
+}
